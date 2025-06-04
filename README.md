c8a04a45a2d78797346ad49e8c57759cda5c2b4e
Ragini <79013435+ragini219@users.noreply.github.com>
2025-06-04 18:54:53 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# protfolio
+# aws_snowflake_weather
+This project demonstrates a real-time data pipeline leveraging AWS services like DynamoDB, EventBridge, and Lambda, along with Snowflake. It integrates a weather API to ingest live data into DynamoDB and seamlessly streams it to Snowflake using Snowpipe for further analysis and reporting.
+
+
+Design
+
+![ChatGPT Image Apr 8, 2025, 11_19_01 PM](https://github.com/user-attachments/assets/fc42f406-71f2-49da-8c2f-8b04281debd2)
+
+
+
+Visualize data in Snowflake
+
+![image](https://github.com/user-attachments/assets/32e694b3-4d5b-4baa-ae95-565908e2c3c5)
---
+++ lambda_function_fetch_data.py
@@ -0,0 +1,53 @@
+import json
+from datetime import datetime
+import requests
+import boto3
+from decimal import Decimal
+
+# https://www.weatherapi.com/v1/current.json?key=&q=India&aqi=no&q=London&aqi=no
+
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('weather')
+
+def get_weather_data(city):
+    api_url = "https://api.weatherapi.com/v1/current.json"
+    params = {
+        "q": city,
+        "key" = 
+    }
+    response = requests.get(api_url, params=params)
+    data = response.json()
+    return data
+
+
+def lambda_handler(event, context):
+    cities = ["Chennai","Kashmir"]
+    for city in cities:
+        data = get_weather_data(city)
+
+        temp = data['current']['temp_c']
+        wind_speed = data['current']['wind_mph']
+        wind_dir = data['current']['wind_dir']
+        pressure_mb = data['current']['pressure_mb']
+        humidity = data['current']['humidity']
+
+        print(city,temp,wind_speed,wind_dir,pressure_mb,humidity)
+        current_timestamp = datetime.now().isoformat()
+
+        item = {
+            'city': city,
+            'time': str(current_timestamp),
+            'temp': temp,
+            'wind_speed': wind_speed,
+            'wind_dir': wind_dir,
+            'pressure_mb': pressure_mb,
+            'humidity': humidity
+        }
+
+        item = json.loads(json.dumps(item), parse_float=Decimal)
+
+        table.put_item(
+            Item=item
+            )
+
